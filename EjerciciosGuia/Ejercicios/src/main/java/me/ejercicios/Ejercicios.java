73d09adeed995a328b19784c7354c570728d089e
Angeluxor <idmiguelangel123@gmail.com>
2023-05-24 21:08:02 -0500

    Added some new exercises

--- EjerciciosGuia/Ejercicios/src/main/java/me/ejercicios/Ejercicios.java
+++ EjerciciosGuia/Ejercicios/src/main/java/me/ejercicios/Ejercicios.java
@@ -6,12 +6,11 @@ package me.ejercicios;
 
 import java.util.Scanner;
 
+
 /**
  *
  * @author idmig
  * 
- * Implementar un programa que le pida dos números enteros a usuario y determine si ambos o
-alguno de ellos es mayor a 25.
  */
 public class Ejercicios {
 
@@ -23,7 +22,11 @@ public class Ejercicios {
         
         System.out.println("Por favor elija el ejercicio que desea realizar"
         + "\n 1. Comprobar si dos enteros son mayores a 25"
-        + "\n 2. Imprimir el tipo de motor según la opciónn seleccionada");
+        + "\n 2. Imprimir el tipo de motor según la opciónn seleccionada"
+        + "\n 3. Pedir una nota y comprobar que sea válida"
+        + "\n 4. Pedir 20 números enteros y sumarlos mientras se cumplan ciertas condiciones"
+        + "\n 5. Leer 4 números e imprimir tantos asteriscos como indique el valor de cada uno"
+        + "\n 6. Codificar una frase");
 
         switch (opt = scann.nextInt()) {
             case 1:
@@ -31,6 +34,21 @@ public class Ejercicios {
                 break;
             case 2:
                 motor();
+                break;
+            case 3:
+                notas();
+                break;
+            case 4:
+                leer20();
+                break;
+            case 5:
+                numAsteriscos();
+                break;
+            case 6:
+                scann.nextLine();
+                System.out.println("Por favor ingrese la frase que desea codificar");
+                String phrase = scann.nextLine();
+                System.out.println("La frase codificada es: \n" + maquinaEnigma(phrase));
                 break;
             default:
                 throw new AssertionError();
@@ -78,10 +96,132 @@ public class Ejercicios {
             default:
                 throw new AssertionError();
         }
+                
+    }
+
+    public static void notas() {
+        int nota = 0;
+        Scanner scann = new Scanner(System.in);
+
+        while (nota < 0 || nota >= 10) {
+            System.out.println("Por favor ingrese una nota entre 0 y 10");
+            nota = scann.nextInt();
+        }
+        System.out.println("Nota ingresada de manera correcta");
+    }
+    
+    public static void leer20() {
+        int n, suma = 0, i = 0;
+        Scanner scann = new Scanner(System.in);
+        do {
+            System.out.println("Por favor ingrese un número positivo");
+            n = scann.nextInt();
+            i++;
+            if (n < 0) {
+                System.out.println(n + "No es un número positivo y no se sumará");
+                continue;
+            }
+            suma = suma + n;
+        } while (n != 0 && i < 20);
+        if (n == 0) {
+            System.out.println("Se capturó el número 0 y la suma de los números válidos ingresados es " + suma);
+        } else {
+            System.out.println("Se han ingresado 20 números y la suma de los números válidos ingresados es " + suma);
+        }
+    }
+    
+    public static void numAsteriscos(){
+        int n1=0, n2=0, n3=0, n4=0;
+        String stg = "*";
+                
+        Scanner scann = new Scanner(System.in);
         
+        for (int i = 1; i <= 4; i++) {
+            System.out.println("Por favor ingrese el " + i + "º valor");
+            switch (i) {
+                case 1:
+                    n1 = scann.nextInt();
+                    break;
+                case 2:
+                    n2 = scann.nextInt();
+                    break;
+                case 3:
+                    n3 = scann.nextInt();
+                    break;
+                case 4:
+                    n4 = scann.nextInt();
+                    break;    
+                default:
+                    throw new AssertionError();
+            }
+        }
+        System.out.println("Los valores ingresados son:");
         
-    }    
+       for (int i = 1; i <= 4; i++) {            
+            switch (i) {
+                case 1:
+                    System.out.print(n1 + " ");
+                    for (int j = 0; j < n1; j++) {
+                        System.out.print("*");
+                    }
+                        System.out.println("");
+                    break;
+                case 2:
+                    System.out.print(n2 + " ");
+                    for (int j = 0; j < n2; j++) {
+                        System.out.print("*");
+                    }
+                    System.out.println("");
+                    break;
+                case 3:
+                    System.out.print(n3 + " ");
+                    for (int j = 0; j < n3; j++) {
+                        System.out.print("*");
+                    }
+                    System.out.println("");
+                    break;
+                case 4:
+                    System.out.print(n4 + " ");
+                    for (int j = 0; j < n4; j++) {
+                        System.out.print("*");
+                    }
+                    System.out.println("");
+                    break;    
+                default:
+                    throw new AssertionError();
+            }
+        }
         
+    }
+    
+    public static String maquinaEnigma(String phrase){
+        String codedPhrase = "";
+        phrase = phrase.toLowerCase();
+        
+        for (int i = 0; i < phrase.length(); i++) {
+            switch (phrase.charAt(i)) {
+                case 'a':
+                    codedPhrase = codedPhrase.concat("@");
+                    break;
+                case 'e':
+                    codedPhrase = codedPhrase.concat("#");
+                    break;
+                case 'i':
+                    codedPhrase = codedPhrase.concat("$");
+                    break;
+                case 'o':
+                    codedPhrase = codedPhrase.concat("%");
+                    break;
+                case 'u':
+                    codedPhrase = codedPhrase.concat("*");
+                    break;
+                default:
+                    codedPhrase = codedPhrase.concat(phrase.substring(i,i+1));
+            }
+        }
+        return codedPhrase;
+        
+    }
 
 }
 
