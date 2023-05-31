6c5773c43528f5250eba416ae5011953ca53f498
Angeluxor <idmiguelangel123@gmail.com>
2023-05-30 19:10:20 -0500

    inniatial commit and push

---
+++ EjerciciosJavaIntro10a13/BuclesEj10a13/src/main/java/me/buclesej10a13/BuclesEj10a13.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package me.buclesej10a13;
+
+/**
+ *
+ * @author idmig
+ */
+public class BuclesEj10a13 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
